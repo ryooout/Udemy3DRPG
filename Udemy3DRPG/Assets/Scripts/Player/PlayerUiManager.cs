24c900e7658864dc1709917ca33ed5257e489f3e
ryooout <ryota01010104@gmail.com>
2022-09-21 23:47:04 +0900

    ko-mitt

---
+++ Udemy3DRPG/Assets/Scripts/Player/PlayerUiManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+//DoTweenを使用するための宣言
+using DG.Tweening;
+public class PlayerUiManager : MonoBehaviour
+{
+    //Hpゲージ
+    public Slider hpSlider;
+    //スタミナゲージ
+    public Slider staminaSlider;
+    /// <summary>
+    /// MaxのHpを設定
+    /// </summary>
+    /// <param name="playerManager"></param>
+    public void Init(PlayerManager playerManager)
+    {
+        hpSlider.maxValue = playerManager.maxHp;
+        hpSlider.value = playerManager.maxHp;
+        staminaSlider.maxValue = playerManager.maxStamina;
+        staminaSlider.value = playerManager.maxStamina;
+    }
+    /// <summary>DoTweenを用いたHpの減少</summary>
+    public void UpdateHp(int hp)
+    {
+        hpSlider.DOValue(hp, 0.5f);
+    }
+    /// <summary>DoTweenを用いたスタミナの減少</summary>
+    public void UpdateStamina(int sutamina)
+    {
+        staminaSlider.DOValue(sutamina, 0.5f);
+    }
+}
